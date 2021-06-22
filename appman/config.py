b3d8b3062563b73600e4d9af34bc80cc4178a5ca
Basilio Bogado <541149+basiliskus@users.noreply.github.com>
2021-06-22 11:39:48 -0700

    remove fixtures test folder and use data/packages instead

--- appman/config.py
+++ appman/config.py
@@ -1,16 +1,22 @@
 import os
 from pathlib import Path
 
-APPMAN_DIR = os.path.dirname(os.path.abspath(__file__))
+OS_SUPPORTED = ["linux", "windows", "darwin"]
 
+DATA_PKG = "data"
+CONFIG_RES_YAML = "config.yaml"
+FORMULAS_RES = "formulas"
+PACKAGES_RES = "packages"
+PM_RES = "package-managers"
+
+APPMAN_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = Path(APPMAN_DIR).parent
 
-DATA_DIR = os.path.join(ROOT_DIR, "data")
 SCHEMAS_DIR = os.path.join(ROOT_DIR, "schemas")
 TESTS_DIR = os.path.join(ROOT_DIR, "tests")
+DATA_DIR = os.path.join(APPMAN_DIR, DATA_PKG)
 
-FORMULAS_DIR = os.path.join(DATA_DIR, "formulas")
-PM_DIR = os.path.join(DATA_DIR, "package-managers")
-CONFIG_PATH = os.path.join(DATA_DIR, "config.yaml")
-
-PACKAGES_DIR = os.path.join(TESTS_DIR, "fixtures")
+PACKAGES_DIR = os.path.join(DATA_DIR, PACKAGES_RES)
+FORMULAS_DIR = os.path.join(DATA_DIR, FORMULAS_RES)
+PM_DIR = os.path.join(DATA_DIR, PM_RES)
+CONFIG_PATH = os.path.join(DATA_DIR, CONFIG_RES_YAML)
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,51 +5,22 @@ import pytest
 from appman import config
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--packages-path", action="store", help="Path to packages definition folder"
-    )
-    parser.addoption(
-        "--config",
-        action="store",
-        default="config.yaml",
-        help="Name for the config file",
-    )
-
-
 @pytest.fixture
-def packages_root(request):
-    ppath = request.config.getoption("--packages-path")
-    return os.path.join(config.ROOT_DIR, ppath)
-
-
-@pytest.fixture
-def config_file(request):
-    cpath = request.config.getoption("--config")
-    return os.path.join(config.ROOT_DIR, cpath)
-
+def config_path():
+    return config.CONFIG_PATH
 
 @pytest.fixture
 def data_root():
     return config.DATA_DIR
 
-
 @pytest.fixture
 def schemas_root():
     return config.SCHEMAS_DIR
 
-
 @pytest.fixture
-def config_path(config_file):
-    return os.path.join(config.DATA_DIR, config_file)
-
+def packages_root():
+    return config.PACKAGES_DIR
 
 @pytest.fixture
 def svars():
     return ["tags"]
-
-
-# def pytest_generate_tests(metafunc):
-#     option_value = metafunc.config.option.datapath
-#     if "datapath" in metafunc.fixturenames and option_value is not None:
-#         metafunc.parametrize("datapath", [option_value])
